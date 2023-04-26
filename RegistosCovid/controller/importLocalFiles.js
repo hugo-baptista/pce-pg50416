04ee043564123bcbb0ea87250e9cb8b38f781fa9
hugo-baptista <pg50416@alunos.uminho.pt>
2023-04-26 11:56:28 +0100

    Start RegistoCovid

---
+++ RegistosCovid/controller/importLocalFiles.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+// Controladores Aqui
+
+module.exports.readFile = async () => {
+    const filePath = 'C:\\Google Drive\\Universidade\\4º Ano\\ApInf-PCE\\PCE\\RegistosCovid\\content\\';
+    const filenames = ['cod_postal.csv', 'doentes.csv', 'registos_covid19.csv'];
+
+    for(let name of filenames) {
+        const fileRead = fs.readFileSync(filePath+name);
+        let lines = fileRead.toString().split('\n');
+        for(let line of lines) {
+            let lineParams = line.split(';');
+            if(lineParams[lineParams.lenght-1].includes('\r')) {
+
+            }
+        }
+    }
+}
---
+++ RegistosCovid/model/Doente.js
@@ -0,0 +1,14 @@
+var mongoose=require('mongoose');
+var CodigoPostal = require('./CodigoPostal');
+var Schema = mongoose.Schema;
+
+var DoenteSchema =  new Schema({
+    id_paciente: {type: Number, unique:true},
+    nome: {type: String},
+    data_nascimento: {type: String},
+    genero: {type: String},
+    codigo_postal: {type: CodigoPostal}
+})
+
+module.exports = mongoose.model('Doente', DoenteSchema)
+// modelo de mongoose ( nome do modelo, modelo em si )
