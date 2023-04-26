557f0632b05d52333b804c51dd2dea52d01b9979
hugo-baptista <pg50416@alunos.uminho.pt>
2023-04-26 12:40:27 +0100

    Connect to MongoDB

--- RegistosCovid/app.js
+++ RegistosCovid/app.js
@@ -6,11 +6,23 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var cors = require('cors');
+
+const mongoose = require('mongoose');
+const uri = "mongodb://localhost:9000/RegistoCovid";
+
 var app = express();
 
+app.use(cors())
+
+mongoose.set('strictQuery', true);
+mongoose.connect(uri)
+  .then(() => console.log('Connected.'))
+  .catch(() => console.log('Error connecting to MongoDB.'))
+
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '50mb', extended: true}));
+app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
